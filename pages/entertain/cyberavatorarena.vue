2ac9c1bec6cfde43316a84459bb1c77c901b8db7
LostAbaddon <lostabaddon@foxmail.com>
2022-08-16 23:22:14 +0800

    增加游戏入口

---
+++ pages/entertain/cyberavatorarena.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="viewer">跳转中，请稍后……</div>
+</template>
+
+<style scoped>
+.viewer {
+	margin-top: 20px;
+	margin-bottom: 20px;
+	text-align: center;
+}
+</style>
+
+<script>
+export default {
+	name: "CyberAvatorArena",
+	async mounted () {
+		location.href = '/games/cyberavatorarena/index.html';
+	},
+}
+</script>
